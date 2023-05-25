937c272beb2a17716b0694445353c1d89d93f887
khoialang <117073964+khoialang@users.noreply.github.com>
2023-05-25 16:44:56 +0700

    change model employee

--- lib/models/employee.dart
+++ lib/models/employee.dart
@@ -1,23 +1,26 @@
+import 'dart:math';
+
 import 'package:flutter_app/models/position.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'branch.dart';
 import 'department.dart';
 
 class Employee {
-  String id;
+  String? id;
   String name;
   String gender;
   DateTime birthDate;
   String address;
   String phoneNumber;
   String email;
-  Department department;
+  String department;
   Position position;
   Branch branch;
   double salary;
 
   Employee({
-    required this.id,
+    this.id,
     required this.name,
     required this.gender,
     required this.birthDate,
@@ -29,4 +32,37 @@ class Employee {
     required this.branch,
     required this.salary,
   });
+
+  Employee.fromJson(Map<String, Object?> json) : this(
+    name: json['name']! as String,
+    gender: json['gender']! as String,
+    birthDate: json['birthDate']! as DateTime,
+    address: json['address']! as String,
+    phoneNumber: json['phoneNumber']! as String,
+    email: json['email']! as String,
+    department: json['department']! as String,
+    position: json['position']! as Position,
+    branch: json['branch']! as Branch,
+    salary: json['salary']! as double,
+  );
+
+  Map<String, Object?> toJson(){
+    return{
+      'name': name,
+      'gender': gender,
+      'birthDate': birthDate,
+      'address': address,
+      'phoneNumber': phoneNumber,
+      'email': email,
+      'department': department,
+      'position': position,
+      'branch': branch,
+      'salary': salary,
+    };
+  }
+
+  final employeeRef = FirebaseFirestore.instance.collection('employees')
+    .withConverter(fromFirestore: (snapshot, _) => Employee.fromJson(snapshot.data()!), 
+    toFirestore: (employee, _) => employee.toJson(),
+  );
 }
